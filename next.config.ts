3c9e4c656e420eb6911b88e5bf13aeff7e9b5e08
Amin Jamali <53099680+BIGboss248@users.noreply.github.com>
2026-09-25 23:27:03 +0330

    fix: nextjs fix

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        "meetjamali.com",
+        "*.meetjamali.com",
+        "localhost:3000",
+        "127.0.0.1:3000",
+      ],
+    },
+  },
   outputFileTracingIncludes: {
     "/*": ["./node_modules/@swc/helpers/**/*"],
   },
